37b3e97c1d38a0f75ba51f82d68ea44ab6b4d0a9
Steve Li <steve@itworx.com.au>
2025-02-03 13:42:21 +1100

    fix an issue of gh-page deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import ProtectedRoute from "./components/ProtectedRoute";
+
 function App() {
   return (
     <Router>
